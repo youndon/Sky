5ca549d2a1386d5c0c59d8907d9c872554be159a
don <bourntivoli@gmail.com>
2022-02-21 20:57:41 +0100

    21/02/22

--- app/src/main/java/com/example/sky/Main.kt
+++ app/src/main/java/com/example/sky/Main.kt
@@ -1,125 +1,41 @@
 package com.example.sky
 
-import androidx.compose.animation.animateColor
+import android.annotation.SuppressLint
+import android.util.Log
+ import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.ScrollableState
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
+@OptIn(DelicateCoroutinesApi::class)
 fun main() {
 
-
 }
 
-@Composable
-fun Coloring() {
-    val cr = remember { mutableStateOf(Color.Cyan) }
-
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.DarkGray)
-    ) {
-        Card(
-            modifier = Modifier
-                .size(500.dp, 400.dp)
-                .clickable {
-                    cr.value = Color(
-                        Random.nextFloat(),
-                        Random.nextFloat(),
-                        Random.nextFloat()
-                    )
-                },
-            backgroundColor = cr.value
-        ) {
-
-        }
-    }
-}
-@Composable
-fun Snaking() {
-    var textState by remember { mutableStateOf("") }
-    val scaffoldState = rememberScaffoldState()
-    val scope = rememberCoroutineScope()
-    Scaffold(
-        scaffoldState = scaffoldState,
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.DarkGray)
-        ) {
-            TextField(
-                value = textState,
-                onValueChange = {
-                    textState = it
-                }
-            )
-            Spacer(Modifier.height(30.dp))
-            Button({
-                scope.launch {
-                    scaffoldState.snackbarHostState.showSnackbar(textState,"dismiss",SnackbarDuration.Long)
-                }
-            }) {
-                Text("Showing!!")
-            }
-        }
-    }
-}
-@Composable
-fun Effect() {
-    val scaffold = rememberScaffoldState()
-    Scaffold(scaffoldState = scaffold) {
-//        var c by remember { mutableStateOf(0) }
-        val c = produceState(0){ delay(2000) ;value = 11 }
-        if (c.value % 2 == 0 && c.value != 0){
-            LaunchedEffect(scaffold.snackbarHostState) {
-                scaffold.snackbarHostState.showSnackbar("The Count's $c")
-            }
-        }
-        Button({ }){
-            Text("count! ${c.value}")
-        }
-    }
-}
-@Composable
-fun Animate() {
-    var size by remember { mutableStateOf(100.dp) }
-    val animateSize by animateDpAsState(
-        targetValue = size,
-//        tween(200,300, FastOutSlowInEasing)
-    animationSpec = spring(Spring.DampingRatioMediumBouncy)
-    )
-        val color by rememberInfiniteTransition().animateColor(
-        initialValue = Color.Cyan,
-        targetValue = Color.White,
-        animationSpec = infiniteRepeatable(
-            animation = tween(),
-            repeatMode = RepeatMode.Restart
-        )
-    )
-    Box(
-        modifier = Modifier
-            .size(animateSize)
-            .background(color)
-            .clickable {
-                size += 50.dp
-            }
-    ) {
-
-    }
-}
